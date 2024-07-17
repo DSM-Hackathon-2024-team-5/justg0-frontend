03333899be0e5b9174cbbd2003254c2184d7e42d
jikwan <jikwan0327@gmail.com>
2024-07-17 10:11:33 +0900

    back 컴포넌트 추가

---
+++ src/components/back.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import BeforeLogo from "../assets/before.svg";
+
+export const Back = ({ children }) => {
+  return (
+    <Container>
+      <Before src={BeforeLogo} />
+      <Text>{children}</Text>
+      <div style={{ width: 32 }}></div>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 8px 12px;
+  box-sizing: border-box;
+  background-color: ${({ theme }) => theme.color.white};
+`;
+
+const Before = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+
+const Text = styled.span`
+  color: #000;
+  font-family: Roboto;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+  letter-spacing: 0.15px;
+`;
